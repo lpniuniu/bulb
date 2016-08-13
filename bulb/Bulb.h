20d7d718bc52c7bbae56d0993225f520d7b9ad6f
hzfanfei <hzfanfei@corp.netease.com>
2016-08-13 13:56:24 +0800

    add some comment

--- bulb/Bulb.h
+++ bulb/Bulb.h
@@ -11,14 +11,49 @@
 
 @interface Bulb : NSObject
 
+/*!
+ *  @brief 注册信号
+ *
+ *  @param signalIdentifier 信号唯一标识
+ *  @param block            信号回调，信号fire只执行一次，如果需要再注册
+ */
 + (void)regiseterSignal:(NSString *)signalIdentifier block:(BulbBlock)block;
+
+/*!
+ *  @brief 注册信号
+ *
+ *  @param signalIdentifiers 信号唯一标识组，全部fire触发回调
+ *  @param block             信号回调，信号fire只执行一次，如果需要再注册
+ */
 + (void)regiseterSignals:(NSArray *)signalIdentifiers block:(BulbBlock)block;
+
+/*!
+ *  @brief 注册信号
+ *
+ *  @param signalIdentifier 信号唯一标识
+ *  @param foreverblock     信号回调, 回调保留，信号fire就执行
+ */
 + (void)regiseterSignal:(NSArray *)signalIdentifier foreverblock:(BulbBlock)foreverblock;
+
+/*!
+ *  @brief 注册信号
+ *
+ *  @param signalIdentifier 信号唯一标识
+ *  @param foreverblock     信号回调, 回调保留，信号fire就执行
+ */
 + (void)regiseterSignals:(NSArray *)signalIdentifiers foreverblock:(BulbBlock)foreverblock;
 
+// Todo regiseterSignal 其他触发条件
+
+/*!
+ *  @brief 在信号发生后执行，如果已发送立即执行，否则延后执行
+ */
 + (void)runAfterSignal:(NSString *)signalIdentifier block:(BulbBlock)block;
 + (void)runAfterSignals:(NSArray *)signalIdentifiers block:(BulbBlock)block;
 
+/*!
+ *  @brief 发出信号
+ */
 + (void)fire:(NSString *)signalIdentifier data:(id)data;
 
 @end
